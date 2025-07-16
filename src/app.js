9aca8e8c7f0cbe6f2a72e72b8d7314128cbbbca1
Rebecca Okoko <okokorebecca39@gmail.com>
2025-07-16 15:08:04 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const routes = require('./routes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
+});
 
