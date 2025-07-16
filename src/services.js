1e20ce8defdb8f5348cb5e290004f616aa023317
Rebecca Okoko <okokorebecca39@gmail.com>
2025-07-16 15:11:12 +0300

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,1 +1,30 @@
+const axios = require('axios');
+const config = require('./config');
+
+const sendMessage = async (to, message) => {
+  try {
+    const response = await axios.post(
+      'https://waba.360dialog.io/v1/messages',
+      {
+        to: to,
+        type: 'text',
+        text: {
+          body: message,
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${config.apiKey}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error sending message:', error.message);
+    throw error;
+  }
+};
+
+module.exports = { sendMessage };
 
